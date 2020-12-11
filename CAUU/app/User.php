c99516d9f4b89e57447a46b25cf3e179f1a70902
Goizane <goizaneterreros@gmail.com>
2020-12-11 13:03:36 +0100

    Configuracion passport.

--- CAUU/app/User.php
+++ CAUU/app/User.php
@@ -5,10 +5,11 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HashApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, HashApiTokens;
 
     /**
      * The attributes that are mass assignable.
