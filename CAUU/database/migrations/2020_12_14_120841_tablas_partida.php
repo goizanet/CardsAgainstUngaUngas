4852d9e56c0f3a748a3c8d4ee248021a2d026b3f
AitorBasarrate <46562527+AitorBasarrate@users.noreply.github.com>
2020-12-15 09:57:23 +0100

    migraciones tablas partida

--- CAUU/database/migrations/2020_12_14_120841_tablas_partida.php
+++ CAUU/database/migrations/2020_12_14_120841_tablas_partida.php
@@ -13,7 +13,41 @@ class TablasPartida extends Migration
      */
     public function up()
     {
-        //
+        if(!Schema::hastable('partida')){
+            Schema::create('partida', function (Blueprint $table) {
+                $table->id();
+                //
+                $table->tinyint('nivel');
+                $table->int('puntuacion');
+                //
+                $table->unsignedBigInteger('jugador_id');
+                $table->foreign('jugador_id')->references('id')->on('jugadores');
+                //
+                $table->timestamps();
+            });
+        }
+
+        if(!Schema::hastable('partida_dato')){
+            Schema::create('partida_dato', function(Blueprint $table){
+                $table->unsignedBigInteger('partida_id');
+                $table->foreign('partida_id')->references('id')->on('partida');
+
+                $table->unsignedBigInteger('dato_id');
+                $table->foreign('dato_id')->references('id')->on('datos');
+            });
+        }
+
+        if(!Schema::hastable('partida_mujer')){
+            Schema::create('partida_mujer', function(Blueprint $table){
+                $table->boolean('en_partida');
+
+                $table->unsignedBigInteger('partida_id');
+                $table->foreign('partida_id')->references('id')->on('partida');
+
+                $table->unsignedBigInteger('mujer_id');
+                $table->foreign('mujer_id')->references('id')->on('mujer');
+            });
+        }
     }
 
     /**
