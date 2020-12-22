3b6add17438c6eba9b00d9c0399e14deb3713a44
AitorBasarrate <46562527+AitorBasarrate@users.noreply.github.com>
2020-12-22 12:30:58 +0100

    pantallas admin

---
+++ CAUU/resources/views/components/gestionarMujeres.php
@@ -0,0 +1,21 @@
+<div>
+    <h1>Panel de gestion de mujeres</h1>
+    <label for="newField">Añadir nueva mujer</label>
+    <input v-model="text" id="newField" placeholder="Introduce el nombre y apellido de la nueva mujer"></input>
+    <input v-model="text" id="newField" placeholder="Introduce el ambito de la nueva mujer"></input>
+    <input v-model="date" id="newField" placeholder="Introduce la fecha de nacimiento de la nueva mujer"></input>
+    <input v-model="date" id="newField" placeholder="Introduce la fecha de fallecimiento de la nueva mujer"></input>
+    <input v-model="date" id="newField" placeholder="Introduce la zona geografica de la nueva mujer"></input>
+    <input v-model="date" id="newField" placeholder="Introduce el continente de la nueva mujer"></input>
+    <input v-model="date" id="newField" placeholder="Introduce la foto de la nueva mujer"></input>
+</div>
+
+<!--<script>-->
+<!--    export default {-->
+<!--        name: "gestionarMujeres"-->
+<!--    }-->
+<!--</script>-->
+<!---->
+<!--<style scoped>-->
+<!---->
+<!--</style>-->
